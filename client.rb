998cf33e70b4d51c1570c563ca0fd37130f48025
godsgift <elton@localhost.localdomain>
2015-02-14 18:34:35 -0800

    User input added, random number of msges for each client added, working on timer (not yet done)

--- client.rb
+++ client.rb
@@ -1,26 +1,38 @@
 require 'socket'
 
-threads = []
+puts "How many clients would you like to connect?"
+client = gets.chomp.to_i
 
-client = 2
+puts "What is the maximum number of messages you want the client to send?"
+puts "Note: the number of messages will always be a random number between 0 and the number you chose"
+maxcounter = gets.chomp.to_i
+counter = 0
 serveraddress = "192.168.38.146"
 port = 9000
-msgcounter = rand(0..5)
 
+threads = []
 #Sets the amount of times a thread will be created
 client.times do |t|
 	#New threads are created
 	threads << Thread.new(t) do |th|
-			socket = TCPSocket.new serveraddress, port
-			
-			#Allow each clients to send different amounts of messages (Not yet done)
-			for i in 0..msgcounter
-			socket.puts "test"
-
+		#Sets the random number generator between 0 and the number the user chose
+		msgcounter = rand(1..maxcounter)
+		socket = TCPSocket.new serveraddress, port
+		#Start timer
+		timestart = Time.now.to_f
+		#Allow each clients to send different amounts of messages	
+		(1..msgcounter).each do |i|
+			counter = counter + 1
+			#Send a string to the server
+			socket.puts "Hello " + counter.to_s
 			line = socket.gets
-			STDOUT.puts line
-			end
-			sleep
+			puts line
+		end
+		#End timer
+		timeend = Time.now.to_f
+		seconds = timeend - timestart
+		puts seconds
+		sleep
 	end
 end
 
