d5e4ac8bd7d9ddbd53c6e5e10056c540af4fa64f
godsgift <elton@localhost.localdomain>
2015-02-16 13:08:52 -0800

    Client update

--- client.rb
+++ client.rb
@@ -1,4 +1,7 @@
 require 'socket'
+require 'thread'
+require 'thwait'
+require 'logger'
 
 puts "How many clients would you like to connect?"
 client = gets.chomp.to_i
@@ -7,18 +10,20 @@ puts "What is the maximum number of messages you want the client to send?"
 puts "Note: the number of messages will always be a random number between 0 and the number you chose"
 maxcounter = gets.chomp.to_i
 
+i = 0
 counter = 0
 serveraddress = "192.168.38.146"
 port = 9000
 
 threads = []
-#Sets the amount of times a thread will be created
-client.times do |t|
+
+while i < client
+	i += 1
 	#New threads are created
-	threads << Thread.new(t) do |th|
+	threads = Thread.fork() do
 		#Sets the random number generator between 0 and the number the user chose
 		msgcounter = rand(1..maxcounter)
-		socket = TCPSocket.new serveraddress, port
+		socket = TCPSocket.open(serveraddress, port)
 		#Start timer
 		timestart = Time.now.to_f
 		#Allow each clients to send different amounts of messages	
@@ -27,8 +32,8 @@ client.times do |t|
 			#Send a string to the server
 			msg = "Hello #{counter}"
 			puts msg
-			puts counter
-			socket.puts msg
+			puts socket
+			counter.puts msg
 			line = socket.gets
 			puts line
 			sleep(0.3)
@@ -41,6 +46,5 @@ client.times do |t|
 		#Put the thread to sleep so that it does not close the connection
 		sleep
 	end
-
 end
-threads.each { |th| th.join }
+ThreadsWait.all_waits(*threads)
