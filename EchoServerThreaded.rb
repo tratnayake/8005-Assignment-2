6813d2ebb4c77299bdb983bbe24b348991148fad
godsgift <elton@localhost.localdomain>
2015-02-18 16:15:18 -0800

    more client

--- EchoServerThreaded.rb
+++ EchoServerThreaded.rb
@@ -16,31 +16,29 @@ puts "Ready to receive on "+ ip +":" + port
 $counter = 0
 
 
-
 while 1
     Thread.fork(server.accept) do |client|
     connection.push(client)
+    puts connection.length
     client = "#{host}:#{port}"
-    puts "#{connection.length} is connected"
+    puts "#{connection} is connected"
     $counter=$counter + 1
     puts $counter.to_s+" clients connected"
-    $logger.info "#{connection.length} has connected"
+    $logger.info "#{client} has connected"
     $logger.info $counter.to_s+" clients connected"
 
     begin
       loop do
         line = client.readline
-        #puts "#{client} says: #{line}"
-	#puts line.chomp.size
         client.puts(line)
       end
     rescue EOFError
       client.close
       $counter=$counter - 1
     
-      puts "#{connection.length} has disconnected"
+      puts "#{client} has disconnected"
         puts $counter.to_s+" clients connected"
-        $logger.info "#{connection.length} has disconnected"
+        $logger.info "#{client} has disconnected"
         $logger.info $counter.to_s+" clients connected"
     end
   end
